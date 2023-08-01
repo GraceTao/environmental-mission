630bc19462a1c172003c53f81fa5a62c9910b6d3
achou1044 <64855502+achou1044@users.noreply.github.com>
2023-08-01 17:11:40 -0400

    Map Clue Background + Icon Issue Fixed
    Background is showing, events are working, map-clue links from main page. Need to change positions of all icons, display tables as pngs and add clue submissions + hints.

--- src/tasks/map-clue/map-home.jsx
+++ src/tasks/map-clue/map-home.jsx
@@ -1,39 +1,35 @@
-import { useEffect, useState, React } from "react";
+import { useState } from "react";
 import {
    Typography,
    Box,
    IconButton,
    Button,
-   Tooltip,
+   //Tooltip,
    Dialog,
    useTheme,
    useMediaQuery,
 } from "@mui/material";
-import Instructions from "../../../components/Instructions";
-import TopBar from "../../../components/TopBar";
-import Instr from "../Instr";
-import AssignmentTwoToneIcon from "@mui/icons-material/AssignmentTwoTone";
+import Instructions from "../../components/Instructions";
+import TopBar from "../../components/TopBar";
+import Instr from "../water-quality/Instr";
+//import AssignmentTwoToneIcon from "@mui/icons-material/AssignmentTwoTone";
 import ChatIcon from "@mui/icons-material/Chat";
 import AirQuality from "./AirQuality";
-
-
-const mapbg = (
-   <svg width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink">
-         <rect width="53" height="53" fill="url(#pattern0)" />
-         <defs>
-            <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
-               <use xlink: href="#image0_297_83" transform="scale(0.0208333)" />
-            </pattern>
-            <image id="image0_297_83" width="48" height="48" xlink: href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAB5ElEQVRoge2XvW4TQRSFvxkvKNihpKPIIiioeQGj0NJR0yIhRaKkDLxBiCJEB7wCgi4ChERBSRcl2FvwU6WKjYzI6lBEIDKzu3Fsr8erzFfeubM6x7tz5hoikUhksUmHIh0qtIwybGgB0xINhMZ4ldN+7/2O/4w50vg3EA2EZjoDYn1GOiZmcgNinazzaIZaJmLyFDopfcqe83ffSetjcsbPwALQeAOR0Ew/C80To2/0li//X2rYGdAnt9IsA7LP3ZJvQPo1FzGnRn367dduNfH6DD+AlXlIOoZ7A7tnUTwGk7vbCj4h+2G2ymaA+EzWeVm05KfQykEXY9/WLmpsJNS6SXbhfdGq/wayi++QXtWua2zM0zLxUJZCib0P7NclaXzMDud/PqzqKDaw1/6K4S4o5KW2j7G32bl0UNXkG0gHG3SV0Ou8wfCkNnmV6DvSLXpLu/9KXSWkwy23s3iUkHbBbmH1EfECuF6r3mNohDF3GLWPgqQ9SDlsrSKtYbnqxm2zZiHw7otmjRIFFIwS3EP6Xb5FOTIPgvU5FP+BvjK6hvI1DKtIR2OFTB+jbWyyyZelvaB9laTDZ9zQuYpfokU62AjW51CeQsZukufbHC5ngJ8GQJC+mEILRkyhxUuhSORs8QeR51l42L2HtwAAAABJRU5ErkJggg==" />
-         </defs>
-      </svg>
-)
+import Ship from "./Ship";
+import Star from "./Star";
+import Tree from "./Tree";
+import Hydrogen from "./Hydrogen";
+import CO2 from "./CO2";
+import chat from "./map_chat_animation.mp4"
+//import MapBackground from "./MapBackground";
 
 function MapInstructions() {
+   const hasEnabledInstr = sessionStorage.getItem("hasEnabledMapInstr");
+   const [openInstr, setOpenInstr] = useState(Boolean(hasEnabledInstr));
 
    const theme = useTheme();
    const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
-
+  
    return openInstr ? (
       <Box
          display="flex"
@@ -79,12 +75,25 @@ function MapInstructions() {
             <div>
                <video controls autoPlay style={{ width: "90%" }}>
                   <source
-                     src={map_chat_animation}
+                     src={chat}
                      alt="text messages"
                      type="video/mp4"
                   />
                </video>
             </div>
+            <Button
+                  variant="contained"
+                  sx={{
+                     backgroundColor: "#417B88",
+                     "&:hover": { backgroundColor: "#4AB0C7 " },
+                  }}
+                  onClick={() => {
+                     setOpenInstr(true);
+                     sessionStorage.setItem("hasEnabledMapInstr", true);
+                  }}
+               >
+                  Visit the Port of Corpus Christi!
+               </Button>
          </Box>
       </div>
    );
@@ -96,13 +105,12 @@ export default function MapHome() {
          Instructions
       </Typography>
    );
-   const [openClipboard, setOpenClipboard] = useState(false);
    const [openMessages, setOpenMessages] = useState(false);
 
    return (
       <Box
          sx={{
-            backgroundImage: mapbg,
+            backgroundImage: "url('https://upload.wikimedia.org/wikipedia/commons/d/d8/Hafen_Corpus_Christi.svg')", //<MapBackground />,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
@@ -134,28 +142,6 @@ export default function MapHome() {
          />
 
          <Box display="flex" flexDirection="row" justifyContent="space-between">
-            <Box display="flex" justifyContent="flex-start" flexDirection="row">
-               <IconButton
-                  sx={{
-                     backgroundColor: "lightgray",
-                     position: { top: 5, left: 5 },
-                     borderRadius: 5,
-                     width: 70,
-                     height: 75,
-                     mr: 3,
-                     mb: 1,
-                     "&:hover": { backgroundColor: "white" },
-                  }}
-                  onClick={() => setOpenClipboard(true)}
-               >
-                  <Tooltip title="Clipboard" arrow>
-                     <AssignmentTwoToneIcon
-                        sx={{ fontSize: 65, color: "black" }}
-                     />
-                  </Tooltip>
-               </IconButton>
-            </Box>
-
             <Box>
                <IconButton onClick={() => setOpenMessages(true)}>
                   <ChatIcon sx={{ fontSize: 55, color: "lightgreen" }} />
@@ -164,7 +150,7 @@ export default function MapHome() {
             <Dialog open={openMessages} onClose={() => setOpenMessages(false)}>
                <video controls autoPlay style={{ width: "100%" }}>
                   <source
-                     src={map_chat_animation}
+                     src={chat}
                      alt="text messages"
                      type="video/mp4"
                   />
