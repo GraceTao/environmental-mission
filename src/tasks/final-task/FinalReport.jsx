a3e6542f5b897b7b3787e150ec872f28e2c15975
GraceTao <graceytao2003@gmail.com>
2023-08-10 16:38:56 -0400

    added error alert for final report

--- src/tasks/final-task/FinalReport.jsx
+++ src/tasks/final-task/FinalReport.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {
+   Alert,
    Box,
    TextField,
    FormLabel,
@@ -27,6 +28,8 @@ export default function FinalReport({
       q5: "",
    });
 
+   const [showError, setShowError] = useState(false);
+
    const handleChange = (e) => {
       const { name, value } = e.target;
 
@@ -34,27 +37,32 @@ export default function FinalReport({
    };
 
    const handleSubmit = async () => {
-      const prevData = JSON.parse(sessionStorage.getItem("allFormData"));
-      let filtered = Object.keys(answers.q2).filter((key) => answers.q2[key] == true);
+      let filtered = Object.keys(answers.q2).filter(
+         (key) => answers.q2[key] == true
+      );
       filtered = filtered.sort();
       filtered = filtered.join(",");
-      const allData = { ...prevData, ...answers, q2: filtered };
-      console.log(allData);
-
       if (
          answers.q1 &&
-         answers.q2 != {} &&
+         filtered &&
          answers.q3 &&
          answers.q4 &&
          answers.q5
       ) {
+         const prevData = JSON.parse(sessionStorage.getItem("allFormData"));
+         const allData = { ...prevData, ...answers, q2: filtered };
+         console.log(allData);
          setShowSubmissionPage(!showSubmissionPage);
+         setShowError(false);
+
          // try {
          //    await axios.post("/api/submituserdata", allData);
          //    console.log("Data added successfully");
          // } catch (err) {
          //    console.log("Error:", err);
          // }
+      } else {
+         setShowError(true);
       }
    };
 
@@ -64,115 +72,124 @@ export default function FinalReport({
       <Box
          sx={{
             backgroundColor: "white",
-            width: "50%",
+            width: { xs: "80%", sm: "70%", md: "60%", lg: "50%" },
             minWidth: 300,
-            p: 5,
-            pt: 3,
             boxShadow: 15,
             border: "solid blueviolet 4px",
             borderRadius: 3,
          }}
       >
-         <Typography
-            fontSize="1.5rem"
-            fontFamily="Courier"
-            align="center"
-            fontWeight="bold"
-         >
-            ENVIRONMENTAL REPORT
-         </Typography>
-         <Typography align="center" fontFamily="Courier" fontSize="1.1rem">
-            Port of Corpus Christi
-         </Typography>
-         <Box display="flex" flexDirection="column" alignItems="flex-start">
-            <FormLabel sx={{ color: "black" }}>Question 1</FormLabel>
-            <RadioGroup
-               name="q1"
-               value={answers.q1}
-               onChange={(e) => setAnswers({ ...answers, q1: e.target.value })}
-               sx={{ ml: 2, m: 1, mb: 4 }}
+         {showError && (
+            <Alert severity="error" variant="filled" sx={{ position: "fixed", zIndex: 3, boxShadow: 10 }}>
+               <Typography fontSize="1.05rem" align="center">
+                  One or more questions have not been answered.
+               </Typography>
+            </Alert>
+         )}
+         <Box sx={{ p: 5, pt: 3 }}>
+            <Typography
+               fontSize="1.5rem"
+               fontFamily="Courier"
+               align="center"
+               fontWeight="bold"
             >
-               {q1.map((choice) => (
-                  <FormControlLabel
-                     key={choice.id}
-                     value={choice.id}
-                     control={<Radio color="success" />}
-                     label={choice.label}
-                  />
-               ))}
-            </RadioGroup>
-            <FormLabel sx={{ color: "black" }}>Question 2</FormLabel>
-            <FormGroup sx={{ ml: 2, m: 1 }}>
-               {q2.map((choice) => (
-                  <FormControlLabel
-                     key={choice.id}
-                     control={
-                        <Checkbox
-                           name={choice.id}
-                           checked={answers.q2[choice.id] || false}
-                           onChange={(e) => {
-                              const prev = answers.q2;
-                              const { name, checked } = e.target;
-                              setAnswers({
-                                 ...answers,
-                                 q2: { ...prev, [name]: checked },
-                              });
-                           }}
-                        />
-                     }
-                     label={choice.label}
-                  ></FormControlLabel>
-               ))}
-            </FormGroup>
-            <br />
-            <Typography>{q3}</Typography>
-            <br />
-            <TextField
-               name="q3"
-               label="Your response here"
-               multiline
-               rows={3}
-               onChange={handleChange}
-               sx={textFieldStyle}
-            />
-            <br />
-            <Typography>{q4}</Typography>
-            <br />
-            <TextField
-               name="q4"
-               label="Your response here"
-               multiline
-               rows={3}
-               onChange={handleChange}
-               sx={textFieldStyle}
-            />
-            <br />
-            <Typography>{q5}</Typography>
-            <br />
-            <TextField
-               name="q5"
-               label="Your response here"
-               multiline
-               rows={3}
-               onChange={handleChange}
-               sx={textFieldStyle}
-            />
-         </Box>
-         <Box align="center">
-            <Button
-               variant="contained"
-               onClick={handleSubmit}
-               sx={{
-                  fontSize: "1.05rem",
-                  width: "25%",
-                  backgroundColor: "blueviolet",
-                  "&:hover": {
-                     backgroundColor: "#9A54E5 "
+               ENVIRONMENTAL REPORT
+            </Typography>
+            <Typography align="center" fontFamily="Courier" fontSize="1.1rem">
+               Port of Corpus Christi
+            </Typography>
+            <Box display="flex" flexDirection="column" alignItems="flex-start">
+               <FormLabel sx={{ color: "black" }}>Question 1</FormLabel>
+               <RadioGroup
+                  name="q1"
+                  value={answers.q1}
+                  onChange={(e) =>
+                     setAnswers({ ...answers, q1: e.target.value })
                   }
-               }}
-            >
-               submit
-            </Button>
+                  sx={{ ml: 2, m: 1, mb: 4 }}
+               >
+                  {q1.map((choice) => (
+                     <FormControlLabel
+                        key={choice.id}
+                        value={choice.id}
+                        control={<Radio color="success" />}
+                        label={choice.label}
+                     />
+                  ))}
+               </RadioGroup>
+               <FormLabel sx={{ color: "black" }}>Question 2</FormLabel>
+               <FormGroup sx={{ ml: 2, m: 1 }}>
+                  {q2.map((choice) => (
+                     <FormControlLabel
+                        key={choice.id}
+                        control={
+                           <Checkbox
+                              name={choice.id}
+                              checked={answers.q2[choice.id] || false}
+                              onChange={(e) => {
+                                 const prev = answers.q2;
+                                 const { name, checked } = e.target;
+                                 setAnswers({
+                                    ...answers,
+                                    q2: { ...prev, [name]: checked },
+                                 });
+                              }}
+                           />
+                        }
+                        label={choice.label}
+                     ></FormControlLabel>
+                  ))}
+               </FormGroup>
+               <br />
+               <Typography>{q3}</Typography>
+               <br />
+               <TextField
+                  name="q3"
+                  label="Your response here"
+                  multiline
+                  rows={3}
+                  onChange={handleChange}
+                  sx={textFieldStyle}
+               />
+               <br />
+               <Typography>{q4}</Typography>
+               <br />
+               <TextField
+                  name="q4"
+                  label="Your response here"
+                  multiline
+                  rows={3}
+                  onChange={handleChange}
+                  sx={textFieldStyle}
+               />
+               <br />
+               <Typography>{q5}</Typography>
+               <br />
+               <TextField
+                  name="q5"
+                  label="Your response here"
+                  multiline
+                  rows={3}
+                  onChange={handleChange}
+                  sx={textFieldStyle}
+               />
+            </Box>
+            <Box align="center">
+               <Button
+                  variant="contained"
+                  onClick={handleSubmit}
+                  sx={{
+                     fontSize: "1.05rem",
+                     width: "25%",
+                     backgroundColor: "blueviolet",
+                     "&:hover": {
+                        backgroundColor: "#9A54E5 ",
+                     },
+                  }}
+               >
+                  submit
+               </Button>
+            </Box>
          </Box>
       </Box>
    );
