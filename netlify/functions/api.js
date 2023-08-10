8c508282b4c2d881f1957fc40f84372a877154bb
GraceTao <graceytao2003@gmail.com>
2023-08-09 22:33:29 -0400

    update user form display and success alerts

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -7,16 +7,14 @@ const SHEET_ID = "1H0Rs1kbonJtlWkSydnf7D0TmVWr44TP47ZfJQt1tEtE";
 const serviceAccount = {
    type: "service_account",
    project_id: "learn-undef-environ-mission",
-   private_key_id: "ea10f8c9d60f90bb6c77c4ea0dbcbfe8c3c28b69",
+   private_key_id: process.env.PRIVATE_KEY_ID,
    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-   client_email:
-      "env-mission@learn-undef-environ-mission.iam.gserviceaccount.com",
-   client_id: "113578363875197552143",
+   client_email: process.env.CLIENT_EMAIL,
+   client_id: process.env.CLIENT_ID,
    auth_uri: "https://accounts.google.com/o/oauth2/auth",
    token_uri: "https://oauth2.googleapis.com/token",
    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-   client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/env-mission%40learn-undef-environ-mission.iam.gserviceaccount.com",
+   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
    universe_domain: "googleapis.com",
 };
 
