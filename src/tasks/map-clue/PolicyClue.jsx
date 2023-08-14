880b1a0f653f3e82915199d82abe184d2e1aa2a1
GraceTao <graceytao2003@gmail.com>
2023-08-13 23:27:25 -0400

    change account colors

--- src/tasks/map-clue/PolicyClue.jsx
+++ src/tasks/map-clue/PolicyClue.jsx
@@ -1,76 +1,85 @@
 import { useState } from "react";
-import { FormControl, FormLabel, Box, Button, Popper, IconButton, Tooltip, TextField, Dialog } from "@mui/material";
-import FlagIcon from '@mui/icons-material/Flag';
+import {
+   FormControl,
+   FormLabel,
+   Box,
+   Button,
+   Popper,
+   IconButton,
+   Tooltip,
+   TextField,
+   Dialog,
+} from "@mui/material";
+import FlagIcon from "@mui/icons-material/Flag";
 
 export default function PolicyClue() {
+   const [openClue, setOpenClue] = useState(false);
 
-    const [openClue, setOpenClue] = useState(false);
+   const letters = [
+      "letter1",
+      "letter2",
+      "letter3",
+      "letter4",
+      "letter5",
+      "letter6",
+   ];
 
-    const [showLetter, setShowLetter] = useState({
-        letter1: "",
-        letter2: "",
-        letter3: "",
-        letter4: "",
-        letter5: "",
-        letter6: ""
-    })
+   const [showLetter, setShowLetter] = useState({
+      letter1: "",
+      letter2: "",
+      letter3: "",
+      letter4: "",
+      letter5: "",
+      letter6: "",
+   });
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setShowLetter({ ...showLetter, [name]: value });
-    };
+   const handleChange = (event) => {
+      const { name, value } = event.target;
+      setShowLetter({ ...showLetter, [name]: value });
+   };
 
+   return (
+      <div>
+         <IconButton
+            onClick={() => setOpenClue(true)}
+            sx={{
+               borderRadius: 2,
+               backgroundColor: "#ffff8d ",
+               width: 64,
+               height: 64,
+               position: { top: 2, left: 5 },
+               "&:hover": { backgroundColor: "#ffff8d" },
+               marginTop: "10px",
+            }}
+         >
+            <Tooltip title="Find the clue!" arrow>
+               <FlagIcon sx={{ fontSize: 55, color: "#ffd600" }} />
+            </Tooltip>
+         </IconButton>
 
-    return (
-        <div>
-                <IconButton
-                    onClick={() => setOpenClue(true)}
-                    sx={{
-                        borderRadius: 2,
-                        backgroundColor: "#ffff8d ",
-                        width: 64,
-                        height: 64,
-                        position: { top: 2, left: 5 },
-                        "&:hover": { backgroundColor: "#ffff8d" },
-                        marginTop: '10px'
-                    }} >
-                    <Tooltip title="Find the clue!" arrow>
-                        <FlagIcon sx={{ fontSize: 55, color: "#ffd600" }} />
-                    </Tooltip>
-                </IconButton>
+         <Dialog
+            open={openClue}
+            onClose={() => setOpenClue(false)}
+            sx={{ boxShadow: 6 }}
+         >
+            <div style={{ overflow: "auto", maxHeight: "50vh" }}>
+               <FormControl>
+                  <FormLabel>Find the clue!</FormLabel>
 
-
-            <Dialog
-                open={openClue} onClose={() => setOpenClue(false)}
-                sx={{boxShadow: 6}}
-            >
-                <div style={{ overflow: "auto", maxHeight: "50vh" }}>
-                    <FormControl>
-                        <FormLabel>Find the clue!</FormLabel>
-
-                        <TextField label="X" onChange={handleChange} name="letter1" value={showLetter.letter1}>
-                            inputProps={{ maxLength: 1 }}
-                        </TextField>
-                        <TextField label="X" onChange={handleChange} name="letter2" value={showLetter.letter2}>
-                            inputProps={{ maxLength: 1 }}
-                        </TextField>
-                        <TextField label="X" onChange={handleChange} name="letter3" value={showLetter.letter3}>
-                            inputProps={{ maxLength: 1 }}
-                        </TextField>
-                        <TextField label="X" onChange={handleChange} name="letter4" value={showLetter.letter4}>
-                            inputProps={{ maxLength: 1 }}
-                        </TextField>
-                        <TextField label="X" onChange={handleChange} name="letter5" value={showLetter.letter5}>
-                            inputProps={{ maxLength: 1 }}
-                        </TextField>
-                        <TextField label="X" onChange={handleChange} name="letter6" value={showLetter.letter6}>
-                            inputProps={{ maxLength: 1 }}
-                        </TextField>
-                        <Button>Submit</Button>
-                    </FormControl>
-                </div>
-            </Dialog>
-        </div>
-    )
+                  {letters.map((letter) => (
+                     <TextField
+                        key={letter}
+                        label="X"
+                        onChange={handleChange}
+                        name={letter}
+                        value={showLetter[letter]}
+                        inputProps={{ maxLength: 1 }}
+                     ></TextField>
+                  ))}
+                  <Button>Submit</Button>
+               </FormControl>
+            </div>
+         </Dialog>
+      </div>
+   );
 }
-
