b57c277f049dfa10d4d7a2d2dfa79a4067a3e08b
GraceTao <graceytao2003@gmail.com>
2023-08-18 00:43:00 -0400

    update contacts

--- src/homepage/ContactPage.jsx
+++ src/homepage/ContactPage.jsx
@@ -9,7 +9,12 @@ export default function ContactPage({ person }) {
          alignItems="center"
          sx={{ backgroundColor: "#FCF6EC", padding: 2, borderRadius: 2 }}
       >
-         <AccountCircleIcon sx={{ color: person.color, fontSize: "100px" }} />
+         <AccountCircleIcon
+            sx={{
+               color: person.color,
+               fontSize: "100px",
+            }}
+         />
          <Typography fontSize="1.3rem">{person.name}</Typography>
          <Typography fontSize="1.1rem">
             <i>{person.occupation}</i>
