c00a7f3d5f5751d782f036546d7bcb778cde650f
GraceTao <graceytao2003@gmail.com>
2023-08-19 19:46:29 -0400

    install and set up cypress test for concurrent google sheets write requests

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -29,11 +29,60 @@ const auth = new google.auth.GoogleAuth({
 
 const service = google.sheets({ version: "v4", auth });
 
-router.post("/submituserdata", async (req, res) => {
-   const { state, county, school, order, emailAttempts, inspectionAttempts, mapAttempts, wqiAttempts, passwordAttempts, ...rest } = req.body;
-   
+class Mutex {
+   constructor() {
+      this.locked = false;
+      this.queue = [];
+   }
 
-   const toAdd = [state, county, school, order, emailAttempts, inspectionAttempts, mapAttempts, wqiAttempts, passwordAttempts, ...Object.values(rest)];
+   async lock() {
+      if (this.locked) {
+         await new Promise((resolve) => this.queue.push(resolve));
+      } else {
+         this.locked = true;
+      }
+   }
+
+   unlock() {
+      if (this.queue.length > 0) {
+         const nextResolve = this.queue.shift();
+         nextResolve();
+      } else {
+         this.locked = false;
+      }
+   }
+}
+
+const mutex = new Mutex();
+
+router.post("/submituserdata", async (req, res) => {
+   const {
+      state,
+      county,
+      school,
+      order,
+      emailAttempts,
+      inspectionAttempts,
+      mapAttempts,
+      wqiAttempts,
+      passwordAttempts,
+      ...rest
+   } = req.body;
+
+   const toAdd = [
+      state,
+      county,
+      school,
+      order,
+      emailAttempts,
+      inspectionAttempts,
+      mapAttempts,
+      wqiAttempts,
+      passwordAttempts,
+      ...Object.values(rest),
+   ];
+
+   await mutex.lock();
 
    try {
       await service.spreadsheets.values.append({
@@ -50,9 +99,10 @@ router.post("/submituserdata", async (req, res) => {
    } catch (err) {
       console.error("Error adding user data: ", err);
       res.status(500).json({ message: "An error occurred" });
+   } finally {
+      mutex.unlock();
    }
 });
-
 
 api.use("/api", router);
 export const handler = serverless(api);
